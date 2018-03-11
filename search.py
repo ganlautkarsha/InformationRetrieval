e06a4a64bc7991c71d1b3eabb919d541cbbf93d8
Natarajan Chidhambharam <natrajct@gmail.com>
2018-03-11 15:22:27 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import urllib2
+import tokenizer
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
@@ -10,15 +11,9 @@ def main():
 @app.route('/search', methods = ['POST'])
 def search():
     query = request.form['query']
-    #soup = BeautifulSoup(urllib2.urlopen(query))
-    url_list = ['http://www.ics.uci.edu/','http://www.ics.uci.edu/about/','http://www.ics.uci.edu/dept/','http://www.ics.uci.edu/faculty/area/','http://www.ics.uci.edu/grad/admissions/index']
-    titles = {}
-    for item in url_list:
-    	title_soup = BeautifulSoup(urllib2.urlopen(item))
-    	titles[item] = title_soup.title.string
-
-    #return render_template('results.html',query=soup.title.string, url = query, titles = titles)
-    return render_template('results.html',titles = titles)
+    url_list = tokenizer.getqueryResult(query)
+    url_list = ["https://" + s for s in url_list]
+    return render_template('results.html',url_list = url_list)
 
 if __name__ == "__main__":
     app.run()
