2a14bbbf85c37eff074102419d993d96a5ebd832
Utkarsha Jitendra Ganla <uganla@uci.edu>
2018-03-10 23:50:27 -0800

    remove numbers > 1000

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,8 @@ import json
 from bs4 import BeautifulSoup
 import re
 import pr3
+from nltk.tokenize import word_tokenize
+
 class tokenize:
     
     dataFolder="WEBPAGES_CLEAN/"
@@ -57,16 +59,25 @@ class tokenize:
         print(docID)
         return json[str(docID)]
     
+    
+    def processQuery(self,query):
+        tokens = tokenize(query).keys()
+    
+    
     def tokenize(self,data):
         print("in tokenize")
 #         print(data)
-        from nltk.tokenize import word_tokenize
+        
         tokens = word_tokenize(data)
 #         print(tokens)
         dictTokenPosition={}
         for i in range(len(tokens)):
             #convert to lower case
             word=str(tokens[i].lower().encode("ascii","replace"))
+            
+            if(word.isdigit()):
+                if(int(word)>1000):
+                    continue
             
             #keep only alpha num characters
             word = re.sub('[^0-9a-zA-Z]+', '', word)
@@ -80,16 +91,18 @@ class tokenize:
             
             #stemming
             # from nltk.stem.porter import PorterStemmer
-            # from nltk.stem import WordNetLemmatizer
-            # # porter = PorterStemmer()
-            # # word = porter.stem(word)
-            # lemmatiser = WordNetLemmatizer()
-            # word=lemmatiser.lemmatize(word)
+            # porter = PorterStemmer()
+            # word = porter.stem(word)
+            
+            from nltk.stem import WordNetLemmatizer
+            lemmatiser = WordNetLemmatizer()
+            word=lemmatiser.lemmatize(word)
             
             
-            from nltk.stem import SnowballStemmer
-            snowball_Stemmer=SnowballStemmer("english")
-            word=snowball_Stemmer.stem(word)
+#             from nltk.stem import SnowballStemmer
+#             snowball_Stemmer=SnowballStemmer("english")
+#             word=snowball_Stemmer.stem(word)
+            
             # store word and position in dictionary
             positions=dictTokenPosition.get(word,[])
             positions.append(i)
@@ -100,5 +113,5 @@ class tokenize:
         
 tokenizer=tokenize()
 tokenizer.parse()
-indexBuild.buildInvertedIndex(tokenizer.globalDictionary,tokenizer.docIDcount)
+# buildInvertedIndex(tokenizer.globalDictionary,tokenizer.docIDcount)
 # print(tokenizer.getURL("0/100"))
