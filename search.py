85258bdf34379198e9af16e0c43a4625c6fdfa70
Natarajan Chidhambharam <natrajct@gmail.com>
2018-03-14 18:03:45 -0700

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,8 @@
-import urllib2, re
+import urllib2
 import tokenizer
+import re
 from bs4 import BeautifulSoup
+import collections
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
@@ -11,9 +13,35 @@ def main():
 @app.route('/search', methods = ['POST'])
 def search():
     query = request.form['query']
-    url_list = tokenizer.getqueryResult(query)
-    url_list = ["https://" + s for s in url_list]
-    return render_template('results1.html',query = query, url_list = url_list)
+    query_modified = query.split()[0]
+
+    url_dict = collections.OrderedDict()
+    titles = collections.OrderedDict()
+
+    url_dict = tokenizer.getqueryResult(query)
+    
+    for docID,url in url_dict.items():
+        path="WEBPAGES_CLEAN/"+docID
+        file=open(path).read()
+        
+        title_soup = BeautifulSoup(urllib2.urlopen(url),'lxml')
+        titles[url] = [title_soup.title.string]
+        
+        soup = BeautifulSoup(file, 'lxml').get_text()
+        soup = re.sub('\W',' ',soup)
+        soup = soup.lower().split()
+        
+        a = '.'
+        soup = [a] + [a] + [a] + soup
+        soup = soup + [a] + [a] + [a]
+        
+        i = soup.index(query_modified)
+        
+        string1 = ' '.join(str(e) for e in soup[i-3:i])
+        string2 = ' '.join(str(e) for e in soup[i+1:i+4])
+        titles[url].append("..."+string1+" "+soup[i]+" "+string2+"...")
+    
+    return render_template('results.html',query = query,titles = titles)
 
 if __name__ == "__main__":
     app.run()
